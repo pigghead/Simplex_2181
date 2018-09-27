72c2825dac39cfe08b545bd9ab0a617cc04514bb
pigghead <justin.strongarms10@gmail.com>
2018-09-27 02:44:48 -0400

    E05 Deliverable

--- C10_MeshClass/AppClass.cpp
+++ C10_MeshClass/AppClass.cpp
@@ -8,6 +8,14 @@ void Application::InitVariables(void)
 	//Make MyMesh object
 	m_pMesh1 = new MyMesh();
 	m_pMesh1->GenerateCube(1.0f, C_WHITE);
+
+	static int p_mCubeCount = 46;
+
+	for (int i = 0; i < 46; ++i)
+	{
+		myMeshCollection.push_back(new MyMesh());
+		myMeshCollection[i]->GenerateCube(1.0f, C_BLACK);
+	}
 }
 void Application::Update(void)
 {
@@ -25,8 +33,29 @@ void Application::Display(void)
 	// Clear the screen
 	ClearScreen();
 
-	m_pMesh->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), ToMatrix4(m_qArcBall));
-	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3( 3.0f, 0.0f, 0.0f)));
+	matrix4 m4view = m_pCameraMngr->GetViewMatrix();
+	matrix4 m4project = m_pCameraMngr->GetProjectionMatrix();
+
+	matrix4 mScale = glm::scale(IDENTITY_M4, vector3(2.0f, 2.0f, 2.0f));
+	matrix4 mTranslate = glm::translate(vector3(1.0f, 0.0f, 0.0f));
+	static float value = 0.0;
+	value += 0.01;
+
+	matrix4 m4model = mTranslate;
+	m4model = glm::scale(vector3(2.0f, 2.0f, 2.0f));
+
+	m_pMesh->Render(m4project, m4view, m4model);
+
+	// moving all 46 cubes
+	for (int i = 0; i < myMeshCollection.size(); i++)
+	{
+		matrix4 mMoveRight = glm::translate(vector3(1.0f * i + value, 1.0f * i, 0.0f));
+
+		myMeshCollection[i]->Render(m4project, m4view, mMoveRight);
+	}
+
+	//m_pMesh->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), ToMatrix4(m_qArcBall));
+	//m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3( 3.0f, 0.0f, 0.0f)));
 		
 	// draw a skybox
 	m_pMeshMngr->AddSkyboxToRenderList();
@@ -51,6 +80,13 @@ void Application::Release(void)
 		m_pMesh = nullptr;
 	}
 	SafeDelete(m_pMesh1);
+
+	// deallocate memory in the entire mesh collection when finished
+	for (int i = 0; i < myMeshCollection.size(); i++)
+	{
+		SafeDelete(myMeshCollection[i]);
+	}
+
 	//release GUI
 	ShutdownGUI();
 }
--- C10_MeshClass/AppClass.h
+++ C10_MeshClass/AppClass.h
@@ -17,6 +17,8 @@ class Application
 	MyMesh* m_pMesh = nullptr;
 	MyMesh* m_pMesh1 = nullptr;
 
+	std::vector<MyMesh*> myMeshCollection;
+
 	String m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
 private:
 	static ImGuiObject gui; //GUI object
